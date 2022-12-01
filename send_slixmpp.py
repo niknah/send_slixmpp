a0cdb2a07a442e34a2f8dbb14e7586e8c49a8c8a
niknah <github@mail.niknah.com>
2022-12-01 14:23:29 +1100

    Assorted fixes

--- send_slixmpp.py
+++ send_slixmpp.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 
 import sys
-import argparse
-import logging
-import threading
 import os
 file_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(file_path+'/slixmpp')
 
+import argparse  # noqa: E402
+import logging  # noqa: E402
 
-from slixmpp import ClientXMPP
-# from slixmpp.exceptions import IqError, IqTimeout
+from slixmpp import ClientXMPP  # noqa: E402
 
 
 class EchoBot(ClientXMPP):
-
     def __init__(self, jid, password):
         ClientXMPP.__init__(self, jid, password)
 
@@ -34,7 +31,7 @@ class EchoBot(ClientXMPP):
         self.dest_jid = None
         self.message = None
 
-    def setup_echo(self):
+    def start_echo(self):
         self.add_event_handler("message", self.echo_message)
 
     def send_message_lines(self):
@@ -47,7 +44,6 @@ class EchoBot(ClientXMPP):
 
             self.send_message(mto=self.dest_jid, mbody=s)
             upto = uptoNext
-        self.disconnect()
 
     def session_start(self, event):
         self.send_presence()
@@ -55,29 +51,29 @@ class EchoBot(ClientXMPP):
 
         if self.message is not None and self.dest_jid is not None:
             self.send_message_lines()
-#            threading.Timer(2,self.send_message_lines).start()
-
-        # Most get_*/set_* methods from plugins use Iq stanzas, which
-        # can generate IqError and IqTimeout exceptions
-        #
-        # try:
-        #     self.get_roster()
-        # except IqError as err:
-        #     logging.error('There was an error getting the roster')
-        #     logging.error(err.iq['error']['condition'])
-        #     self.disconnect()
-        # except IqTimeout:
-        #     logging.error('Server is taking too long to respond')
-        #     self.disconnect()
+        self.disconnect()
 
     def echo_message(self, msg):
         if msg['type'] in ('chat', 'normal'):
             msg.reply("Thanks for sending\n%(body)s" % msg).send()
 
 
+def start_xmpp(args):
+    user = args.user
+    if 'XMPP_USER' in os.environ:
+        user = os.environ['XMPP_USER']
+    xmpp = EchoBot(
+        user,
+        os.environ['XMPP_PASS']
+        )
+    xmpp.dest_jid = args.dest
+    return xmpp
+
+
 if __name__ == '__main__':
     # Ideally use optparse or argparse to get JID,
     # password, and log level.
+
     run_cmd = 'XMPP_PASS=xxx '+__file__
     parser = argparse.ArgumentParser(
         description="Sends message from stdin to xmpp server",
@@ -89,41 +85,45 @@ if __name__ == '__main__':
         " user@xmpp.server.com touser@xmpp.server.com/*"
     )
     parser.add_argument(
-            "user", type=str,
-            help="from jid xxx@xxmpserver.com/resource")
+        "user",
+        type=str,
+        help="from jid xxx@xxmpserver.com/resource"
+        )
     parser.add_argument(
-            "dest", nargs='?', type=str,
-            help="destination jid xxx@xxmpserver.com/resource")
+        "dest",
+        nargs="?",
+        type=str,
+        help="destination jid xxx@xxmpserver.com/resource"
+        )
     parser.add_argument(
-            "--address", type=str,
-            help="server.com:port")
+        "--address",
+        type=str,
+        help="server.com:port"
+        )
     parser.add_argument(
-            "--echo", action="store_true",
-            help="Echo any messages received")
+        "--echo",
+        action="store_true",
+        help="Echo any messages received"
+        )
     parser.add_argument("--verbose", action="store_true", help="Verbose")
     args = parser.parse_args()
 
     if args.verbose:
         logging.basicConfig(
             level=logging.DEBUG,
-            format='%(levelname)-8s %(message)s')
+            format='%(levelname)-8s %(message)s'
+            )
 
-    user = args.user
-    if 'XMPP_USER' in os.environ:
-        user = os.environ['XMPP_USER']
-    xmpp = EchoBot(
-        user,
-        os.environ['XMPP_PASS']
-        )
-    xmpp.dest_jid = args.dest
     if args.echo:
-        xmpp.setup_echo()
-    elif xmpp.dest_jid is not None:
-        data = sys.stdin.readlines()
-        xmpp.message = data
+        xmpp = start_xmpp(args)
+        xmpp.start_echo()
     else:
-        print("Must mention destination address\n")
-        exit(1)
+        data = sys.stdin.readlines()
+        s = ("\n".join(data)).strip()
+        if len(s) <= 0:
+            exit(0)
+        xmpp = start_xmpp(args)
+        xmpp.message = data
 
     if args.address:
         addressArr = args.address.split(':')
